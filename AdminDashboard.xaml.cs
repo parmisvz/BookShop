f744f838909da777d55ded982b242dc4587c75c4
parmisvz <parmisvaziferm@gmail.com>
2022-07-04 23:52:56 +0430

    Added Project file

---
+++ AdminDashboard.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AP01Project
+{
+    /// <summary>
+    /// Interaction logic for AdminDashboard.xaml
+    /// </summary>
+    public partial class AdminDashboard : Window
+    {
+        public AdminDashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void ViewUserRadiobtn_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddedBooksRadiobtn_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AccountBalanceRadiobtn_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+
+        private void Edit(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
+
